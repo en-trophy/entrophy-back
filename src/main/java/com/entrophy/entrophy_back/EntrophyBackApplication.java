ede9f14e4fa8ceeff307d2221ed9b3a9d006e24c
vvineey <vvin506@naver.com>
2025-12-01 12:34:26 +0900

    chore : 스프링 부트 세팅

---
+++ src/main/java/com/entrophy/entrophy_back/EntrophyBackApplication.java
@@ -0,0 +1,13 @@
+package com.entrophy.entrophy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrophyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrophyBackApplication.class, args);
+	}
+
+}
