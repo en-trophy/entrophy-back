e913e3673553ce3079d18bd04a53def007445dd5
vvineey <vvin506@naver.com>
2025-12-05 19:30:14 +0900

    fix : 필요없는 매서드 삭제

--- src/main/java/com/entrophy/entrophy_back/category/repository/CategoryRepository.java
+++ src/main/java/com/entrophy/entrophy_back/category/repository/CategoryRepository.java
@@ -4,17 +4,8 @@ import com.entrophy.entrophy_back.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
-
 @Repository
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
-    Optional<Category> findByCode(String code);
-
-    List<Category> findAllByIsActiveTrueOrderBySortOrderAsc();
-
-    List<Category> findAllByOrderBySortOrderAsc();
 }
