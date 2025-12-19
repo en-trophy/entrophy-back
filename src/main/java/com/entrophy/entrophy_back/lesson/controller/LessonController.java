4e8a450973c91def332544f49de545b96c7cbdcb
vvineey <vvin506@naver.com>
2025-12-19 23:10:14 +0900

    docs(lesson) : 레슨 API 스웨거 태그 및 설명 추가

--- src/main/java/com/entrophy/entrophy_back/lesson/controller/LessonController.java
+++ src/main/java/com/entrophy/entrophy_back/lesson/controller/LessonController.java
@@ -7,6 +7,7 @@ import com.entrophy.entrophy_back.lesson.dto.response.LessonResponse;
 import com.entrophy.entrophy_back.lesson.service.LessonService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/lessons")
+@Tag(name = "레슨 API", description = "레슨 생성, 전체 조회, 단건 조회, 수정, 카테고리별 조회 기능 제공")
 public class LessonController {
 
     private final LessonService lessonService;
@@ -25,6 +27,7 @@ public class LessonController {
 
     // 레슨 생성
     @PostMapping
+    @Operation(summary = "[관리자용] 레슨 생성", description = "레슨(수어 단어/표현) 데이터를 생성")
     public ResponseEntity<LessonResponse> createLesson(@Valid @RequestBody LessonRequest lessonRequest) {
         LessonResponse created = lessonService.createLesson(lessonRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
@@ -32,6 +35,7 @@ public class LessonController {
 
     // 전체 레슨 조회
     @GetMapping
+    @Operation(summary = "전체 레슨 조회", description = "전체 레슨 목록을 조회")
     public ResponseEntity<List<LessonResponse>> getLessons() {
         List<LessonResponse> lessons = lessonService.getLessons();
         return ResponseEntity.ok(lessons);
@@ -46,6 +50,7 @@ public class LessonController {
 
     // 레슨 조회
     @GetMapping("/{id}")
+    @Operation(summary = "레슨 단건 조회", description = "레슨 ID로 특정 레슨을 조회")
     public ResponseEntity<LessonResponse> getLesson(@PathVariable Long id) {
         LessonResponse lesson = lessonService.getLesson(id);
         return ResponseEntity.ok(lesson);
@@ -53,6 +58,7 @@ public class LessonController {
 
     // 레슨 정보 수정
     @PutMapping("/{id}")
+    @Operation(summary = "[관리자용] 레슨 정보 수정", description = "레슨 ID에 해당하는 레슨 정보를 수정")
     public ResponseEntity<LessonResponse> updateLesson(@PathVariable Long id, @RequestBody LessonRequest lessonRequest) {
         LessonResponse lesson = lessonService.updateLesson(id, lessonRequest);
         return ResponseEntity.ok(lesson);
