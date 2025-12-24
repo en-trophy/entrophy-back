b690214e8703a620ea4af69514f18b24935f7878
vvineey <vvin506@naver.com>
2025-12-25 01:43:06 +0900

    update : 응답 dto에 mediatype enum 추가

--- src/main/java/com/entrophy/entrophy_back/mediaStorage/dto/response/MediaUploadResponse.java
+++ src/main/java/com/entrophy/entrophy_back/mediaStorage/dto/response/MediaUploadResponse.java
@@ -1,6 +1,11 @@
 package com.entrophy.entrophy_back.mediaStorage.dto.response;
 
+import com.entrophy.entrophy_back.mediaStorage.model.MediaType;
+
 public record MediaUploadResponse(
+        MediaType
+        type,
+
         String
         blobName,
 
