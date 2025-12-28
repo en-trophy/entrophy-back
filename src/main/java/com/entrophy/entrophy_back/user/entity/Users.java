a598ba0feeafc138b862a1693cab5d279a0bb40c
vvineey <vvin506@naver.com>
2025-12-29 01:01:13 +0900

    feat : user 엔티티 추가

---
+++ src/main/java/com/entrophy/entrophy_back/user/entity/Users.java
@@ -0,0 +1,33 @@
+package com.entrophy.entrophy_back.user.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String loginId;
+
+    private String password;
+
+    private String name;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    protected Users(){}
+
+
+}
