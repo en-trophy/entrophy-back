1bf33f7db4d76aaee8acaa30e380059db8994f3e
vvineey <vvin506@naver.com>
2026-01-01 20:01:37 +0900

    feat : 생성자로 생성하도록 기능 추가

--- src/main/java/com/entrophy/entrophy_back/learningHistory/entity/LearningHistory.java
+++ src/main/java/com/entrophy/entrophy_back/learningHistory/entity/LearningHistory.java
@@ -2,6 +2,7 @@ package com.entrophy.entrophy_back.learningHistory.entity;
 
 
 import com.entrophy.entrophy_back.category.entity.Category;
+import com.entrophy.entrophy_back.learningHistory.dto.request.LearningHistoryCreateRequest;
 import com.entrophy.entrophy_back.lesson.entity.Lesson;
 import com.entrophy.entrophy_back.user.entity.Users;
 import jakarta.persistence.*;
@@ -44,7 +45,19 @@ public class LearningHistory {
     protected LearningHistory (){}
 
 
+    public LearningHistory(Users user, Category category, Lesson lesson, LearningHistoryCreateRequest learningHistoryCreateRequest) {
+        this.user = user;
+        this.category = category;
+        this.lesson = lesson;
+        this.score = learningHistoryCreateRequest.score();
+        this.practiceSeconds = learningHistoryCreateRequest.practiceSeconds();
+        this.aiFeedback = learningHistoryCreateRequest.aiFeedback();
+    }
 
+    @PrePersist
+    void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 
 
 
